65434efc152ef53cf39987a7860f697e10c32891
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-03 11:51:01 -0600

    attacking heros

--- app.js
+++ app.js
@@ -25,13 +25,54 @@ const boss = {
 //#region Logic
 function attackBoss() {
   let totalDamage = 0
+
   heroes.forEach(hero => {
     totalDamage += hero.damage
   })
+
   boss.health -= totalDamage
+
+  if (boss.health < 1) {
+    boss.health = 0
+  }
+
+  drawBossStats()
+}
+
+function bossAttack() {
+  heroes.forEach(hero => {
+    hero.health -= boss.damage
+  })
+  drawHeroStats()
 }
 //#endregion
 
 //#region Draw
+function drawBossStats() {
+  const bossHealthPercentage = Math.floor(boss.health * boss.maxHealth / 100)
 
+  const bossElem = document.getElementById('boss')
+
+  const progressElem = bossElem.querySelector('.progress')
+  progressElem.ariaValueNow = bossHealthPercentage.toString()
+  const progressBarElem = bossElem.querySelector('.progress-bar')
+  // @ts-ignore
+  progressBarElem.style.width = bossHealthPercentage + '%'
+}
+
+function drawHeroStats() {
+  heroes.forEach(hero => {
+    const heroElem = document.getElementById(hero.name)
+    const heroHealthElem = heroElem.querySelector('.health')
+    // @ts-ignore
+    heroHealthElem.innerText = hero.health
+  })
+}
+//#endregion
+
+// #region Page Load
+
+drawHeroStats()
+
+setInterval(bossAttack, 5000);
 //#endregion
